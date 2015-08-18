969b126c66a672614b3baba595063121fbe4a33c
adkarta <arman.kartawidjaja@gmail.com>
2015-08-18 14:25:37 +0700

    use SoapClient

--- src/TraveldocClient.php
+++ src/TraveldocClient.php
@@ -2,9 +2,9 @@
 
 namespace Adkarta\Traveldoc;
 
-use WS\WSASoap;
+use Adkarta\Traveldoc\SoapClient as SoapClient;
 
-class TraveldocClient extends \SoapClient
+class TraveldocClient 
 {
 
     protected $config;
@@ -15,41 +15,25 @@ class TraveldocClient extends \SoapClient
 
     public function __construct($config = array(), $wsdl = "http://stage.traveldoc-airasia.com/Integrated/4.2/Service.svc?wsdl")
     {
+        
         $this->config = $config;
 
         if ($this->soap_client == null) {
-            $this->soap_client = new \SoapClient($wsdl, array('soap_version' => SOAP_1_2));
+            $this->soap_client = new SoapClient($wsdl, array('soap_version' => SOAP_1_2));
         }
 
-        parent::__construct($wsdl, array('soap_version' => SOAP_1_2));
     }
 
-    public function __doRequest($request, $location, $saction, $version, $one_way = null)
+    public function getSession($sessionMessage)
     {
-        $dom = new \DOMDocument();
-        $dom->loadXML($request);
-
-        $wsa = new WSASoap($dom);
-        $wsa->addAction($saction);
-        $wsa->addTo($location);
-        $wsa->addMessageID();
-        $wsa->addReplyTo();
-
-        $request = $wsa->saveXML();
-
-        return parent::__doRequest($request, $location, $saction, $version, $one_way);
-    }
-
-    public function test()
-    {
-        return "test ok";
-    }
-
-    public function geta()
-    {
-        return $this->soap_client;
+        try {
+            $out = $this->soap_client->GetSession($sessionMessage);
+            $SessionGUID = $out->GetSessionResult;
+            return $SessionGUID;
+        } catch (SoapFault $fault) {
+            // echo json_encode($fault);
+            return false;
+        }
     }
 
 }
-
-?>
