66674e485553a08988e9cd8652ba50c81241deeb
adkarta <arman.kartawidjaja@gmail.com>
2015-09-15 17:04:23 +0700

    Create Object Session

---
+++ src/Session.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Adkarta\Traveldoc;
+
+use Adkarta\Traveldoc\SoapClient as SoapClient;
+
+class Session 
+{
+
+    public $UserName;
+
+    public $Password;
+
+    public $ClientName;
+
+    public $ConfigurationName;
+
+}
